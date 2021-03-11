672e2d330324a85d68b24957412e16675aa268f1
Griffin <you@example.com>
2021-03-11 17:00:01 -0500

    big data storage moves

--- src/main.py
+++ src/main.py
@@ -6,31 +6,36 @@ from clustering import *
 # print("Welcome to my clustering alg!")
 # print("  [1] K-means clustering")
 # print("  [2] Spectral clustering")
-# alg = input("Please enter the number of the algorithm you wish to use: ")
-# while alg != "1" and alg != "2":
-#     alg = input("Whoops! Please only enter '1' or '2' ('q' to quit): ")
-#     if alg == "q":
+# inpt = input("Please enter the number of the algorithm you wish to use: ")
+# while inpt != "1" and inpt != "2":
+#     inpt = input("Whoops! Please only enter '1' or '2' ('q' to quit): ")
+#     if inpt == "q":
 #         break
 
-alg = "1" # REMOVE ME
+inpt = "1" # REMOVE ME
 ## Read in data from text files
 
-
-print("reading in data")
-# file paths
+# define file paths to /data directory
 data_fp = "../data/"
 cho_fp = data_fp + "cho.txt"
 iyer_fp = data_fp + "iyer.txt"
 
-# cho_data = np.loadtxt(cho_fp)
-# iyer_data = np.loadtxt(iyer_fp)
+# Read data from specified file paths into pandas dataframes
+cho_df = pd.read_csv(cho_fp, header=None, sep='\t')
+iyer_df = pd.read_csv(iyer_fp, header=None, sep='\t')
 
-cho_data = pd.read_csv(cho_fp, header=None, sep='\t')
-iyer_data = pd.read_csv(cho_fp, header=None, sep='\t')
+# remove outlier values (denoted as -1 in colum 1)
+cho_df = cho_df[cho_df[1] > 0]
+iyer_df = iyer_df[iyer_df[1] > 0]
 
-# print(iyer_data)
+# Drop the first two and make arrays only with gene attributes 
+cho_arr = cho_df.drop(columns=[0,1]).to_numpy()
+iyer_arr = iyer_df.drop(columns=[0,1]).to_numpy()
 
-if alg == "1":
-    kmeans(cho_data)
-elif alg == "2":
-    spectral(cho_data)
+print(cho_arr)
+print(iyer_arr)
+
+alg = Kmeans(iyer_arr)
+# alg.info()
+# elif inpt == "2":
+#     spectral(cho_data)
