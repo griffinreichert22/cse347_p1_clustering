5eb9cb64583a0101f28cb2f493c535fa9bb6a65a
Griffin <you@example.com>
2021-03-10 21:45:19 -0500

    reading in data from files

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # testing recieving user input from keyboard
 
+## Get user input from command line
 print("Welcome to my clustering alg!")
 print("  [1] K-means clustering")
 print("  [2] Spectral clustering")
@@ -8,7 +9,19 @@ while alg != "1" and alg != "2":
     alg = input("Whoops! Please only enter '1' or '2' ('q' to quit): ")
     if alg == "q":
         break
+## Read in data from text files
 print("reading in data")
+# file paths
+data_fp = "../data/"
+cho_fp = data_fp + "cho.txt"
+iyer_fp = data_fp + "iyer.txt"
+
+cho_file = open(cho_fp, "r")
+cho_data = cho_file.read()
+# print(cho_data)
+iyer_file = open(iyer_fp, "r")
+iyer_data = iyer_file.read()
+# print(iyer_data)
 
 if alg == "1":
     print("Do K-means here")
