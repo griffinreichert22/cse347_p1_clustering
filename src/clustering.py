e01a8cfaff0637a6872df3f5dca2ac90320daa9d
Griffin <you@example.com>
2021-03-10 22:40:48 -0500

    refactored into methods, using numpy to read data

---
+++ src/clustering.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def kmeans(data):
+    print("TODO: K-means")
+    print(data)
+    
+def spectral(data):
+    print("TODO: spectral")
+    print(data)
+
--- src/main.py
+++ src/main.py
@@ -1,29 +1,31 @@
-# testing recieving user input from keyboard
+import numpy as np
+from clustering import *
 
 ## Get user input from command line
-print("Welcome to my clustering alg!")
-print("  [1] K-means clustering")
-print("  [2] Spectral clustering")
-alg = input("Please enter the number of the algorithm you wish to use: ")
-while alg != "1" and alg != "2":
-    alg = input("Whoops! Please only enter '1' or '2' ('q' to quit): ")
-    if alg == "q":
-        break
+# print("Welcome to my clustering alg!")
+# print("  [1] K-means clustering")
+# print("  [2] Spectral clustering")
+# alg = input("Please enter the number of the algorithm you wish to use: ")
+# while alg != "1" and alg != "2":
+#     alg = input("Whoops! Please only enter '1' or '2' ('q' to quit): ")
+#     if alg == "q":
+#         break
+
+alg = "1" # REMOVE ME
 ## Read in data from text files
+
+
 print("reading in data")
 # file paths
 data_fp = "../data/"
 cho_fp = data_fp + "cho.txt"
 iyer_fp = data_fp + "iyer.txt"
 
-cho_file = open(cho_fp, "r")
-cho_data = cho_file.read()
-# print(cho_data)
-iyer_file = open(iyer_fp, "r")
-iyer_data = iyer_file.read()
+cho_data = np.loadtxt(cho_fp)
+iyer_data = np.loadtxt(iyer_fp)
 # print(iyer_data)
 
 if alg == "1":
-    print("Do K-means here")
+    kmeans(cho_data)
 elif alg == "2":
-    print("Do spectral here")
+    spectral(cho_data)
